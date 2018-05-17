1635925bd3042be6b3aeb87575c22ea2b0808568
Mandi Wise <hello@mandiwise.com>
2018-05-17 16:34:20 -0700

    Animate the intro with Greensock.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
+import { TweenLite } from "gsap";
+
+import "./App.css";
 
 class App extends Component {
+  componentDidMount() {
+    TweenLite.fromTo(this.intro, 4.5, { opacity: 0 }, { opacity: 1, delay: 1 });
+    TweenLite.to(this.intro, 1.5, { opacity: 0, delay: 5.5 });
+  }
+
   render() {
     return (
-      <div>
-        <p>Hello, Greensock world!</p>
+      <div className="container">
+        <section className="intro" ref={intro => (this.intro = intro)}>
+          <p>
+            A long time ago, in a galaxy far,<br /> far away....
+          </p>
+        </section>
       </div>
     );
   }
