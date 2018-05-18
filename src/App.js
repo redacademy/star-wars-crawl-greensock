c3de7270f6f0867f743b6363151b07210e885a5f
Mandi Wise <hello@mandiwise.com>
2018-05-18 10:46:54 -0700

    Update to React 16.3 refs.

--- src/App.js
+++ src/App.js
@@ -5,27 +5,34 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.intro = React.createRef();
+    this.logo = React.createRef();
+  }
+
   componentDidMount() {
-    TweenLite.set(this.logo, { opacity: 0 });
+    TweenLite.set(this.logo.current, { opacity: 0 });
     const tl = new TimelineLite();
 
     tl
-      .fromTo(this.intro, 4.5, { opacity: 0 }, { opacity: 1, delay: 1 })
-      .to(this.intro, 1.5, { opacity: 0 })
-      .set(this.logo, { opacity: 1, scale: 2.75 })
-      .to(this.logo, 8, { scale: 0.05, ease: Power2.easeOut })
-      .to(this.logo, 1.5, { opacity: 0 }, "-=1.5");
+      .fromTo(this.intro.current, 4.5, { opacity: 0 }, { opacity: 1, delay: 1 })
+      .to(this.intro.current, 1.5, { opacity: 0 })
+      .set(this.logo.current, { opacity: 1, scale: 2.75 })
+      .to(this.logo.current, 8, { scale: 0.05, ease: Power2.easeOut })
+      .to(this.logo.current, 1.5, { opacity: 0 }, "-=1.5");
   }
 
   render() {
     return (
       <div className="container">
-        <section className="intro" ref={intro => (this.intro = intro)}>
+        <section className="intro" ref={this.intro}>
           <p>
             A long time ago, in a galaxy far,<br /> far away....
           </p>
         </section>
-        <section className="logo" ref={logo => (this.logo = logo)}>
+        <section className="logo" ref={this.logo}>
           <img src={logo} alt="Star Wars logo" />
         </section>
       </div>
