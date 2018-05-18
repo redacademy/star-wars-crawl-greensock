122c95912d375ae0b9c14d294062caf48f1821fa
Mandi Wise <hello@mandiwise.com>
2018-05-18 10:14:45 -0700

    Animate logo scaling and opacity.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from "react";
-import { TimelineLite, TweenLite } from "gsap";
+import { Power2, TimelineLite, TweenLite } from "gsap";
 
+import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
   componentDidMount() {
+    TweenLite.set(this.logo, { opacity: 0 });
     const tl = new TimelineLite();
 
     tl
       .fromTo(this.intro, 4.5, { opacity: 0 }, { opacity: 1, delay: 1 })
-      .to(this.intro, 1.5, { opacity: 0 });
+      .to(this.intro, 1.5, { opacity: 0 })
+      .set(this.logo, { opacity: 1, scale: 2.75 })
+      .to(this.logo, 8, { scale: 0.05, ease: Power2.easeOut })
+      .to(this.logo, 1.5, { opacity: 0 }, "-=1.5");
   }
 
   render() {
@@ -19,6 +24,9 @@ class App extends Component {
           <p>
             A long time ago, in a galaxy far,<br /> far away....
           </p>
+        </section>
+        <section className="logo" ref={logo => (this.logo = logo)}>
+          <img src={logo} alt="Star Wars logo" />
         </section>
       </div>
     );
